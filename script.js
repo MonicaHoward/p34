0b7fbb06848f97e62c5483d0fd1bac1968932d79
Monica Howard <jay.m.howard@gmail.com>
2017-10-16 15:32:12 -0500

    initial commit finished layout

---
+++ script.js
@@ -0,0 +1,20 @@
+function renderGrid(){
+  var blocks = document.getElementsByClassName('content');
+  var pad = 10, cols = 3, newLeft, newTop;
+
+  for (var i = 1; i < blocks.length; i++) {
+    if (i % cols == 0) {
+      newTop = (blocks[i-cols].offsetTop + blocks[i-cols].offsetHeight) + pad;
+      blocks[i].style.top = newTop + "px";
+    } else {
+        if(blocks[i-cols]) {
+          newTop = (blocks[i-cols].offsetTop + blocks[i-cols].offsetHeight) + pad;
+          blocks[i].style.top = newTop + "px";
+        }
+        newLeft = (blocks[i-1].offsetLeft + blocks[i-1].offsetWidth) + pad;
+        blocks[i].style.left = newLeft + "px";
+    }
+  }
+}
+window.addEventListener("load", renderGrid, false);
+window.addEventListener("resize", renderGrid, false);
